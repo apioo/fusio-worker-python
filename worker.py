169c128c210cef224d54233962cdb669479d02fb
Christoph Kappestein <christoph.kappestein@gmail.com>
2026-03-08 17:30:12 +0100

    hdnale action hash

--- worker.py
+++ worker.py
@@ -48,9 +48,21 @@ class Worker:
         return self.new_message(True, "Action successfully deleted")
 
     def get_action_file(self, action: str) -> str:
-        self.assert_action(action)
+        parts = action.split('@')
+        name = parts[0]
+        action_hash = parts[1] if len(parts) > 1 else None
 
-        return self.ACTIONS_DIR + '/' + action + '.py'
+        self.assert_action(name)
+
+        base_dir = os.path.join(self.ACTIONS_DIR, name)
+
+        if not os.path.exists(base_dir):
+            os.makedirs(base_dir)
+
+        if action_hash:
+            return os.path.join(base_dir, f"{action_hash}.py")
+        else:
+            return os.path.join(base_dir, "main.py")
 
     def assert_action(self, action: str):
         if not re.match("^[A-Za-z0-9_-]{3,255}$", action):
